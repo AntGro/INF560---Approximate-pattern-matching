ba21ae6fef09fb713043c1ddacaa274e3a11ec13
Yassin Hamaoui <yassin.hamaoui@nissan.polytechnique.fr>
2019-03-19 19:45:16 +0100

    push

--- apm/test_models.bash
+++ apm/test_models.bash
@@ -3,16 +3,17 @@
 cores=(1 2 4 10 20)
 nodes=(1 2 5 10)
 patterns=('A' 'ACGTTA' 'G' 'AAAC' 'T' 'CT' 'C' 'GTT' 'AGTC' 'CGGTA' 'AA' 'ACCGGT' 'CG' 'ACGGTAC' 'ACT' 'CT' 'ACGTAT' 'AAA' 'ACCCGTTGGAT' 'AAC')
-#A MODIFIER
-#FAIRE UNE LISTE DE PATTERNS A PARCOURIR
-#tester databases
+databases=('dna/line_chrY.fa' 'dna/small_chrY.fa')
+
 
 for n in ${cores[@]}; do
     for N in ${nodes[@]}; do
-        if [N>n]; then
-            for m in `seq 1 20`; do            
-                tmp = ("${patterns[@]:0:m}")
-                salloc -n $n -N $N mpirun ./apm 0 dna/chr1_136K_alt.fa "${tmp[@]}" 
+        if [ $n>=$N ]; then
+            for database in ${databases[@]}; do
+               for m in `seq 1 20`; do
+                    tmp=("${patterns[@]:0:m}")
+                    salloc -n $n -N $N mpirun ./apm 0 $database "${tmp[@]}"
+                done
             done
         fi
     done
