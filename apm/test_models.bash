eb06f55588b3a67df74449dd1c4ef8b3f2cf089a
Yassin Hamaoui <yassin.hamaoui@allemagne.polytechnique.fr>
2019-03-20 13:25:18 +0100

    jdjdjk

--- apm/test_models.bash
+++ apm/test_models.bash
@@ -3,14 +3,14 @@
 cores=(1 2 4 10 20)
 nodes=(1 2 5 10)
 patterns=('A' 'ACGTTA' 'G' 'AAAC' 'T' 'CT' 'C' 'GTT' 'AGTC' 'CGGTA' 'AA' 'ACCGGT' 'CG' 'ACGGTAC' 'ACT' 'CT' 'ACGTAT' 'AAA' 'ACCCGTTGGAT' 'AAC')
-databases=('dna/line_chrY.fa' 'dna/small_chrY.fa')
+databases=('dna/chr6_GL000254v2_alt.fa')
 
 
 for n in ${cores[@]}; do
     for N in ${nodes[@]}; do
-        if [ $N -lt $n ]; then
+        if [ $N -le $n ]; then
             for database in ${databases[@]}; do
-               for m in `seq 1 20`; do
+               for m in `seq 1 19`; do
                     tmp=("${patterns[@]:0:m}")
                     echo  salloc -n $n -N $N mpirun ./apm 0 $database "${tmp[@]}"
                     salloc -n $n -N $N mpirun ./apm 0 $database "${tmp[@]}"
